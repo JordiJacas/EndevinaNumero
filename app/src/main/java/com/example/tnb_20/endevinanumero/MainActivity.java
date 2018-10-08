3bc96a8bcbf1da9aac66968eaae58ba02feaa609
JordiJacas <jjacasventura@iesesteveterradas.cat>
2018-10-08 20:16:58 +0200

    enter

--- app/src/main/java/com/example/tnb_20/endevinanumero/MainActivity.java
+++ app/src/main/java/com/example/tnb_20/endevinanumero/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnKeyListener;
+
 
 import java.util.Random;
 
@@ -67,8 +70,38 @@ public class MainActivity extends AppCompatActivity {
                     nIntents = 0;
                     number = rNumber.nextInt(100) + 1;
                 }
+            }
+        });
 
+        text.setOnKeyListener(new OnKeyListener() {
 
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // TODO Auto-generated method stub
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    int iNumber = Integer.parseInt(text.getText().toString());
+                    if(number < iNumber){
+                        //textV.setText("El numero es menor de " + iNumber);
+                        Toast toast = Toast.makeText(getApplicationContext(),"El numero es menor de " + iNumber, Toast.LENGTH_SHORT);
+                        toast.show();
+                        text.setText("");
+                        nIntents++;
+                    }else if (number > iNumber){
+                        // textV.setText("El numero es mayor de " + iNumber);
+                        Toast toast = Toast.makeText(getApplicationContext(),"El numero es mayor de " + iNumber, Toast.LENGTH_SHORT);
+                        toast.show();
+                        text.setText("");
+                        nIntents++;
+                    }else if(number == iNumber){
+                        //textV.setText("Has adivinado el numero " + iNumber + ", Numero de intentos: " + nIntents + ", Torna a començar");
+                        Toast toast = Toast.makeText(getApplicationContext(),"Has endevinat el numero " + iNumber + "/n Numero de intents: " + nIntents, Toast.LENGTH_SHORT);
+                        toast.show();
+                        text.setText("");
+                        nIntents = 0;
+                        number = rNumber.nextInt(100) + 1;
+                    }
+                }
+                return false;
             }
         });
     }
